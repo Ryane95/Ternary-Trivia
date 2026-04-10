a1c74afacdbead0664aa860c31d70a952ebd2117
Ryan Guy Edimo <edimongomryan@gmail.com>
2026-04-10 15:42:25 -0800

    Intro screen

--- screens.cpp
+++ screens.cpp
@@ -5,11 +5,24 @@
  * Version: 1.0
  ***************************/
 #include "screens.h"
+#include "quizgame.h"
 #include <iomanip>
 #include <iostream>
 #include <string>
 using namespace std;
 
-void screens::introScreen() {}
+void screens::introScreen()
+{
+cout << "===================================================================================\n";
+cout << "|                                                                                  |\n";
+cout << "|                             Welcome to Jeopardy!                                 |\n";
+for (int counter =0; counter <5; counter++){
+    cout << "|                                                                                  |\n";
+}
+cout << "===================================================================================\n";
+pauseGame();
+}
 void screens::questionScreen(vector<vector<string>> questions, string category,
-                             int index) {}
+                             int index) {
+
+}
